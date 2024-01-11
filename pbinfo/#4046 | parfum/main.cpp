0c0285f9bb992e4420f315429557ef31d8e78812
woob123 <142715769+woob123@users.noreply.github.com>
2024-01-11 14:05:37 +0200

    Create main.cpp

---
+++ pbinfo/#4046 | parfum/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n, x, z;
+    cin >> n >> x >> z;
+    int count[100001], days[100001];
+    for(int i = 1; i <= n; i++){
+        cin >> count[i] >> days[i];
+    }    
+}
